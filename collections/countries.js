098ee0d42e3ec53887c447e968f149b4fe943113
Michael Alexander <beefsack@gmail.com>
2013-05-17 14:54:56 +1000

    Cleaned up data and added participant photos.

--- collections/countries.js
+++ collections/countries.js
@@ -13,57 +13,244 @@ Meteor.methods({
 			throw new Meteor.Error(401, "Only admins can initialise countires");
 		}
 
-		var countries = [
-			{name: 'Albania', alpha2Code: 'AL'},
-			{name: 'Andorra', alpha2Code: 'AD'},
-			{name: 'Armenia', alpha2Code: 'AM'},
-			{name: 'Austria', alpha2Code: 'AT'},
-			{name: 'Azerbaijan', alpha2Code: 'AZ'},
-			{name: 'Belarus', alpha2Code: 'BY'},
-			{name: 'Belgium', alpha2Code: 'BE'},
-			{name: 'Bosnia and Herzegovina', alpha2Code: 'BA'},
-			{name: 'Bulgaria', alpha2Code: 'BG'},
-			{name: 'Croatia', alpha2Code: 'HR'},
-			{name: 'Cyprus', alpha2Code: 'CY'},
-			{name: 'Czech Republic', alpha2Code: 'CZ'},
-			{name: 'Denmark', alpha2Code: 'DK'},
-			{name: 'Estonia', alpha2Code: 'EE'},
-			{name: 'Finland', alpha2Code: 'FI'},
-			{name: 'France', alpha2Code: 'FR'},
-			{name: 'Georgia', alpha2Code: 'GE'},
-			{name: 'Germany', alpha2Code: 'DE'},
-			{name: 'Greece', alpha2Code: 'GR'},
-			{name: 'Hungary', alpha2Code: 'HU'},
-			{name: 'Iceland', alpha2Code: 'IS'},
-			{name: 'Ireland', alpha2Code: 'IE'},
-			{name: 'Israel', alpha2Code: 'IL'},
-			{name: 'Italy', alpha2Code: 'IT'},
-			{name: 'Latvia', alpha2Code: 'LV'},
-			{name: 'Lithuania', alpha2Code: 'LT'},
-			{name: 'Luxembourg', alpha2Code: 'LU'},
-			{name: 'Macedonia', alpha2Code: 'MK'},
-			{name: 'Malta', alpha2Code: 'MT'},
-			{name: 'Moldova', alpha2Code: 'MD'},
-			{name: 'Monaco', alpha2Code: 'MC'},
-			{name: 'Montenegro', alpha2Code: 'ME'},
-			{name: 'Morocco', alpha2Code: 'MA'},
-			{name: 'Netherlands', alpha2Code: 'NL'},
-			{name: 'Norway', alpha2Code: 'NO'},
-			{name: 'Poland', alpha2Code: 'PL'},
-			{name: 'Portugal', alpha2Code: 'PT'},
-			{name: 'Romania', alpha2Code: 'RO'},
-			{name: 'Russia', alpha2Code: 'RU'},
-			{name: 'San Marino', alpha2Code: 'SM'},
-			{name: 'Serbia', alpha2Code: 'RS'},
-			{name: 'Slovakia', alpha2Code: 'SK'},
-			{name: 'Slovenia', alpha2Code: 'SI'},
-			{name: 'Spain', alpha2Code: 'ES'},
-			{name: 'Sweden', alpha2Code: 'SE'},
-			{name: 'Switzerland', alpha2Code: 'CH'},
-			{name: 'Turkey', alpha2Code: 'TR'},
-			{name: 'Ukraine', alpha2Code: 'UA'},
-			{name: 'United Kingdom', alpha2Code: 'GB'}
-		];
+		var countries = {
+			2013: {
+				AL: {
+					id: 'AL',
+					name: 'Albania',
+					artist: 'Adrian Lulgjuraj & Bledar Sejko',
+					song: 'Identitet'
+				},
+				AM: {
+					id: 'AM',
+					name: 'Armenia',
+					artist: 'Dorians',
+					song: 'Lonely Planet'
+				},
+				AT: {
+					id: 'AT',
+					name: 'Austria',
+					artist: 'Natália Kelly',
+					song: 'Shine'
+				},
+				AZ: {
+					id: 'AZ',
+					name: 'Azerbaijan',
+					artist: 'Farid Mammadov',
+					song: 'Hold Me'
+				},
+				BY: {
+					id: 'BY',
+					name: 'Belarus',
+					artist: 'Alyona Lanskaya',
+					song: 'Solayoh'
+				},
+				BE: {
+					id: 'BE',
+					name: 'Belgium',
+					artist: 'Roberto Bellarosa',
+					song: 'Love Kills'
+				},
+				BG: {
+					id: 'BG',
+					name: 'Bulgaria',
+					artist: 'Elitsa Todorova, Stoyan Yankulov',
+					song: 'Samo Shampioni (Only Champions)'
+				},
+				HR: {
+					id: 'HR',
+					name: 'Croatia',
+					artist: 'Klapa s mora',
+					song: 'Mižerja'
+				},
+				CY: {
+					id: 'CY',
+					name: 'Cyprus',
+					artist: 'Despina Olympiou',
+					song: 'An Me Thimasai'
+				},
+				DK: {
+					id: 'DK',
+					name: 'Denmark',
+					artist: 'Emmelie de Forest',
+					song: 'Only Teardrops'
+				},
+				EE: {
+					id: 'EE',
+					name: 'Estonia',
+					artist: 'Birgit',
+					song: 'Et Uus Saaks Alguse'
+				},
+				FI: {
+					id: 'FI',
+					name: 'Finland',
+					artist: 'Krista Siegfrids',
+					song: 'Marry Me'
+				},
+				FR: {
+					id: 'FR',
+					name: 'France',
+					artist: 'Amandine Bourgeois',
+					song: 'L\'enfer Et Moi'
+				},
+				GE: {
+					id: 'GE',
+					name: 'Georgia',
+					artist: 'Nodi Tatishvili & Sophie Gelovani',
+					song: 'Waterfall'
+				},
+				DE: {
+					id: 'DE',
+					name: 'Germany',
+					artist: 'Cascada',
+					song: 'Glorious'
+				},
+				GR: {
+					id: 'GR',
+					name: 'Greece',
+					artist: 'Koza Mostra feat. Agathon Iakovidis',
+					song: 'Alcohol Is Free'
+				},
+				HU: {
+					id: 'HU',
+					name: 'Hungary',
+					artist: 'ByeAlex',
+					song: 'Kedvesem (Zoohacker Remix)'
+				},
+				IS: {
+					id: 'IS',
+					name: 'Iceland',
+					artist: 'Eythor Ingi',
+					song: 'Ég Á Líf'
+				},
+				IE: {
+					id: 'IE',
+					name: 'Ireland',
+					artist: 'Ryan Dolan',
+					song: 'Only Love Survives'
+				},
+				IL: {
+					id: 'IL',
+					name: 'Israel',
+					artist: 'Moran Mazor',
+					song: 'Rak Bishvilo'
+				},
+				IT: {
+					id: 'IT',
+					name: 'Italy',
+					artist: 'Marco Mengoni',
+					song: 'L\'Essenziale'
+				},
+				LV: {
+					id: 'LV',
+					name: 'Latvia',
+					artist: 'PeR',
+					song: 'Here We Go'
+				},
+				LT: {
+					id: 'LT',
+					name: 'Lithuania',
+					artist: 'Andrius Pojavis',
+					song: 'Something'
+				},
+				MK: {
+					id: 'MK',
+					name: 'F.Y.R. Macedonia',
+					artist: 'Esma & Lozano',
+					song: 'Pred Da Se Razdeni'
+				},
+				MT: {
+					id: 'MT',
+					name: 'Malta',
+					artist: 'Gianluca',
+					song: 'Tomorrow'
+				},
+				MD: {
+					id: 'MD',
+					name: 'Moldova',
+					artist: 'Aliona Moon',
+					song: 'O Mie'
+				},
+				ME: {
+					id: 'ME',
+					name: 'Montenegro',
+					artist: 'Who See',
+					song: 'Igranka'
+				},
+				NL: {
+					id: 'NL',
+					name: 'Netherlands',
+					artist: 'Anouk',
+					song: 'Birds'
+				},
+				NO: {
+					id: 'NO',
+					name: 'Norway',
+					artist: 'Margaret Berger',
+					song: 'I Feed You My Love'
+				},
+				RO: {
+					id: 'RO',
+					name: 'Romania',
+					artist: 'Cezar',
+					song: 'It\'s My Life'
+				},
+				RU: {
+					id: 'RU',
+					name: 'Russia',
+					artist: 'Dina Garipova',
+					song: 'What If'
+				},
+				SM: {
+					id: 'SM',
+					name: 'San Marino',
+					artist: 'Valentina Monetta',
+					song: 'Crisalide (Vola)'
+				},
+				RS: {
+					id: 'RS',
+					name: 'Serbia',
+					artist: 'Moje 3',
+					song: 'Ljubav Je Svuda'
+				},
+				SI: {
+					id: 'SI',
+					name: 'Slovenia',
+					artist: 'Hannah',
+					song: 'Straight Into Love'
+				},
+				ES: {
+					id: 'ES',
+					name: 'Spain',
+					artist: 'ESDM',
+					song: 'Contigo Hasta El Final (With You Until The End)'
+				},
+				SE: {
+					id: 'SE',
+					name: 'Sweden',
+					artist: 'Robin Stjernberg',
+					song: 'You'
+				},
+				CH: {
+					id: 'CH',
+					name: 'Switzerland',
+					artist: 'Takasa',
+					song: 'You And Me'
+				},
+				UA: {
+					id: 'UA',
+					name: 'Ukraine',
+					artist: 'Zlata Ognevich',
+					song: 'Gravity'
+				},
+				GB: {
+					id: 'GB',
+					name: 'United Kingdom',
+					artist: 'Bonnie Tyler',
+					song: 'Believe In Me'
+				}
+			}
+		};
 
 		// Make sure we have all of the countries of the set
 		_.each(countries, function(c) {
