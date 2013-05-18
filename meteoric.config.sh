2ee61f80fe9dd042c16aa5d92c341223fbd8ee95
Michael Alexander <beefsack@gmail.com>
2013-05-18 15:37:52 +1000

    Added meteoric config.

---
+++ meteoric.config.sh
@@ -0,0 +1,14 @@
+# IP or URL of the server you want to deploy to
+APP_HOST=euro.beefsack.com
+
+# Comment this if your host is not an EC2 instance
+EC2_PEM_FILE=~/.ssh/ec2au.pem
+
+# What's your project's Git repo?
+GIT_URL=git://github.com/beefsack/eurovision-party.git
+
+# Does your project use meteorite, or plain meteor?
+METEORITE=true
+
+# What's your app name?
+APP_NAME=europarty
